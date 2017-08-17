e8baaeaef4611964ba3405a96e943bdf63ffd517
guidao <964704149@qq.com>
2017-08-17 20:51:39 +0800

    add iter

--- src/api/us23.rs
+++ src/api/us23.rs
@@ -4,17 +4,75 @@ use scraper::Html;
 use scraper::Selector;
 use reqwest;
 use std::str::FromStr;
+use IStory;
+use SearchResult;
+use Chapter;
 
 #[derive(Debug)]
-pub struct StoryDir {
+struct StoryDir {
     pub name: String,
     pub link: String,
 }
 
+pub struct US23 {}
+impl US23 {
+    pub fn new() -> US23 {
+        US23{}
+    }
+}
+struct IterChapter {
+    dir :Vec<StoryDir>,
+    cur :i32,
+}
+
+impl Iterator for IterChapter {
+    type Item = Chapter;
+    fn next(&mut self) -> Option<Chapter> {
+        if self.dir.len() <= self.cur as usize{
+            return None;
+        }
+        let link = &self.dir[self.cur as usize].link;
+        let content = get_story_content(&link);
+        self.cur += 1;
+        Some(Chapter{
+            name: self.dir[self.cur as usize].name.clone(),
+            content: content,
+        })
+    }
+}
+
+impl IStory for US23 {
+    fn search(&self, name: &str) ->Vec<SearchResult> {
+        let search_link = format!("http://zhannei.baidu.com/cse/search?q={}&click=1&entry=1&s=5513259216532962936&nsid=", name);
+        let mut resp = reqwest::get(&search_link).unwrap();
+        let mut content = String::new();
+        resp.read_to_string(&mut content);
+        let document = Html::parse_document(&content);
+        let selector = Selector::parse("a[cpos=title]").unwrap();
+        let mut v = Vec::new();
+        for element in document.select(&selector) {
+            let val = element.value();
+            let sd = SearchResult{
+                name: String::from_str(val.attr("title").unwrap_or_default()).unwrap(),
+                link: String::from_str(val.attr("href").unwrap_or_default()).unwrap(),
+            };
+            v.push(sd);
+        }
+        v
+    }
+    fn download(&self, link: &str) -> Box<Iterator<Item = Chapter>>{
+        Box::new(IterChapter{
+            dir: get_story_dir(link),
+            cur: 0,
+        })
+    }
+}
+
+
 pub const ORIGIN_LINK: &str = "http://www.23us.so/files/article/html/1/1247/index.html";
 
-pub fn get_story_dir(link: String) -> Vec<StoryDir> {
-    let mut resp = reqwest::get(&link).unwrap();
+fn get_story_dir(link: &str) -> Vec<StoryDir> {
+    let mut resp = reqwest::get(link).unwrap();
     let mut content = String::new();
     resp.read_to_string(&mut content);
     let document = Html::parse_document(&content);
@@ -46,7 +104,7 @@ pub fn get_story_content(link: &str) -> String {
     return s;
 }
 
-pub fn search_story(name: &str) -> Vec<StoryDir> {
+fn search_story(name: &str) -> Vec<StoryDir> {
     let search_link = format!("http://zhannei.baidu.com/cse/search?q={}&click=1&entry=1&s=5513259216532962936&nsid=", name);
     let mut resp = reqwest::get(&search_link).unwrap();
     let mut content = String::new();
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,59 @@
 extern crate reqwest;
 extern crate scraper;
 extern crate clap;
-
+extern crate url;
 
 mod api;
-use std::str::FromStr;
 use api::us23;
 use clap::{App, SubCommand, Arg};
+use url::Url;
+
+trait IStory {
+    fn search(&self, name: &str) -> Vec<SearchResult>;
+    fn download(&self, link: &str) -> Box<Iterator<Item = Chapter>>;
+}
+
+struct SearchResult {
+    name: String,
+    link: String,
+}
+
+struct Chapter {
+    name: String,
+    content: String,
+}
 
 fn main() {
-    let matches = App::new("story").subcommand(SubCommand::with_name("search")
-                                               .arg(Arg::with_name("name").help("search story")))
-        .subcommand(SubCommand::with_name("get")
-                    .arg(Arg::with_name("link").help("download link")))
+    let matches = App::new("story")
+        .subcommand(SubCommand::with_name("search")
+                        .arg(Arg::with_name("name").help("search story")))
+        .subcommand(SubCommand::with_name("get").arg(Arg::with_name("link").help("download link")))
         .get_matches();
     match matches.subcommand() {
         ("search", Some(search_command)) => {
             let name = search_command.value_of("name").unwrap();
-            println!("search arg:{}", search_command.value_of("name").unwrap());
-            let result = us23::search_story(name);
+            let result = us23::US23::new().search(name);
             for story in result {
                 println!("Name: {}  Link: {}", story.name, story.link);
             }
-        },
+        }
         ("get", Some(get_command)) => {
             let link = get_command.value_of("link").unwrap();
-            let book = us23::get_story_dir(String::from_str(link).unwrap());
-            for chapter in book {
+            let content = build(link).unwrap().download(link);
+            for chapter in content {
                 println!("{}", chapter.name);
-                let s = us23::get_story_content(&chapter.link);
-                println!("{}", s);
+                println!("{}", chapter.content);
             }
         }
         _ => unreachable!(),
     }
+}
+
+fn build(link: &str) -> Option<Box<IStory>> {
+    let url = Url::parse(link).unwrap();
+    let host: &str = url.host_str().unwrap_or_default();
+    if let Some(_) = host.find("23us") {
+        return Some(Box::new(us23::US23::new()));
+    }
+    None
 }
